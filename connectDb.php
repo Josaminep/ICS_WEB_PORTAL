4c86e8f7a63650a5162ac310a454d057d43821fd
josaminep <josaminep@gmail.com>
2024-11-10 18:21:15 +0800

    first commit

--- connectDb.php
+++ connectDb.php
@@ -2,10 +2,12 @@
     $host     = 'localhost';
     $username = 'root';
     $password = '';
-    $dbname   ='ics_db';
+    $dbname   = 'ics_db';
     
     $conn = new mysqli($host, $username, $password, $dbname);
-    if(!$conn){
-        die("Cannot connect to the database.". $conn->error);
+
+    // Check connection
+    if($conn->connect_error){
+        die("Cannot connect to the database. Error: " . $conn->connect_error);
     }
-?>   
+?>
